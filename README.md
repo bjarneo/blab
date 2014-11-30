a1082e65396c252b2b6eacbd4e5668a52f09478b
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-30 19:59:36 +0100

    Blab is blabbing

---
+++ README.md
@@ -0,0 +1,18 @@
+blab
+====
+
+Blab - Node.js chat server.
+<br />
+This project is a learning project. Client will be written later.
+
+## Start server
+```javascript
+node chat.js
+```
+
+## Connect to server
+```bash
+telnet host port
+
+// ex: telnet bjarneo.codes 5000
+```
---
+++ chat.js
@@ -0,0 +1,88 @@
+/* jslint node: true */
+'use strict';
+
+var net = require('net'),
+    clients = [];
+
+//TODO: Split it into lib/resources
+net.createServer(function (socket) {
+    function welcome() {
+        socket.write('Welcome ' + socket.name + '\n');
+        socket.write('To change your name simply type: /name myNickBrah\n');
+    }
+
+    function createClient() {
+        //socket.remoteAddress + ':' + socket.remotePort;
+        socket.name = 'Greenbaby-' + (clients.length + 1);
+
+        clients.push(socket);
+
+        welcome();
+    }
+
+    // Send message to clients
+    function broadcast(message) {
+        each(clients, function(client) {
+            client.write(message);
+        });
+
+        // server output
+        process.stdout.write(message);
+    }
+
+    function changeNick(data) {
+        var oldName = socket.name;
+
+        clients.pop(socket);
+
+        socket.name = data.toString().split(' ')[1].replace(/(\r\n|\n|\r)/gm, '');
+
+        clients.push(socket);
+
+        broadcast('* ' + oldName + ' changed nick to ' + socket.name + ' *\n');
+    }
+
+    function contains(data, text) {
+        return data.toString().indexOf(text);
+    }
+
+    function each(arr, fn) {
+        var len = arr.length, i = 0;
+
+        if (!arr.length && arr instanceof Array) {
+            throw new TypeError('Check your array.');
+        } else if (typeof fn !== 'function') {
+            throw new TypeError('No callback function added.');
+        }
+
+        for (; i < len; i++) {
+            /* jshint validthis: true */
+            fn.call(this, arr[i], i);
+        }
+    }
+
+    // Create our client
+    createClient();
+
+    broadcast(socket.name + ' joined the chat\n');
+
+    // Our chat stream
+    socket.on('data', function (data) {
+        if (contains(data, '/name ') !== -1) {
+            changeNick(data);
+
+            return;
+        }
+
+        broadcast(socket.name + ' > ' + data);
+    });
+
+    // Pop our client when it leaves
+    socket.on('end', function () {
+        clients.splice(clients.indexOf(socket), 1);
+
+        broadcast(socket.name + ' left the chat.\n');
+    });
+}).listen(5000);
+
+console.log('Blab server running at port 5000\n');
