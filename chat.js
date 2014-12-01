48e5c29db7d0580fe196ceeb679c49055fc847d3
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-01 19:26:05 +0100

    Split into files

--- chat.js
+++ chat.js
@@ -2,89 +2,35 @@
 'use strict';
 
 var net = require('net'),
-    clients = [];
+    Welcome = require('./lib/welcome'),
+    Users = require('./lib/users'),
+    Utils = require('./lib/utilities');
 
-//TODO: Split it into lib/resources
 net.createServer(function (socket) {
-    function welcome() {
-        socket.write('Welcome ' + socket.name + '\n');
-        socket.write('To change your name simply type: /name myNickBrah\n');
-    }
-
-    function createClient() {
-        //socket.remoteAddress + ':' + socket.remotePort;
-        socket.name = 'Greenbaby-' + (clients.length + 1);
-
-        clients.push(socket);
-
-        welcome();
-    }
-
     // Send message to clients
     function broadcast(message) {
-        each(clients, function(client) {
-            client.write(message);
+        Utils.each(Users.getUsers(), function(user) {
+            user.write(message);
         });
 
         // server output
         process.stdout.write(message);
     }
 
-    function changeNick(data) {
-        var taken = false,
-            oldName = socket.name,
-            newName = data.toString().split(' ')[1].replace(/(\r\n|\n|\r)/gm, '');
+    // Socket settings
+    socket.setKeepAlive(true);
 
-        // Check if nick aleady exists
-        each(clients, function(client) {
-            if (client.name === newName) {
-                taken = true;
-            }
-        });
+    // Create user
+    Users.createUser(socket);
 
-        if (taken) {
-            broadcast('* Sorry ' + oldName + ' name ' + newName + ' is already in use *\n');
-
-            return;
-        }
-
-        clients.pop(socket);
-
-        socket.name = newName;
-
-        clients.push(socket);
-
-        broadcast('* ' + oldName + ' changed nick to ' + socket.name + ' *\n');
-    }
-
-    function contains(data, text) {
-        return data.toString().indexOf(text);
-    }
-
-    function each(arr, fn) {
-        var len = arr.length, i = 0;
-
-        if (!arr.length && arr instanceof Array) {
-            throw new TypeError('Check your array.');
-        } else if (typeof fn !== 'function') {
-            throw new TypeError('No callback function added.');
-        }
-
-        for (; i < len; i++) {
-            /* jshint validthis: true */
-            fn.call(this, arr[i], i);
-        }
-    }
-
-    // Create our client
-    createClient();
+    // Welcome user
+    Welcome.ahouy(socket);
 
     broadcast(socket.name + ' joined the chat\n');
 
-    // Our chat stream
     socket.on('data', function (data) {
-        if (contains(data, '/name ') !== -1) {
-            changeNick(data);
+        if (Utils.contains(data, '/name ') !== -1) {
+            Users.changeUserName(socket, data, broadcast);
 
             return;
         }
@@ -92,12 +38,13 @@ net.createServer(function (socket) {
         broadcast(socket.name + ' > ' + data);
     });
 
-    // Pop our client when it leaves
+    // Remove the client from the list when it leaves
     socket.on('end', function () {
-        clients.splice(clients.indexOf(socket), 1);
+        Users.getUsers().splice(Users.getUser(socket), 1);
 
         broadcast(socket.name + ' left the chat.\n');
     });
 }).listen(5000);
 
-console.log('Blab server running at port 5000\n');
+// Put a friendly message on the terminal of the server.
+console.log('Chat server running at port 5000\n');
