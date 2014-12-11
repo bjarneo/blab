c5c6b27abb8001895f05503b6a008d054d552f91
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-11 19:55:28 +0100

    Better user structure

--- chat.js
+++ chat.js
@@ -1,53 +1,52 @@
 'use strict';
 
 var net = require('net'),
-    users = require('./lib/users'),
+    userModule = require('./lib/user'),
     commands = require('./lib/commands');
 
 net.createServer(function (socket) {
+    var user = null;
+
     // Socket settings
     socket.setKeepAlive(true);
 
-    // Set socket
-    users.setSocket(socket);
-
     // Add user
-    users.addUser();
+    user = new userModule(socket);
 
     // Welcome and inform user
-    users.messageUser('Welcome ' + socket.name + '');
-    users.messageUser('Type /help for commands');
+    user.messageUser('Welcome ' + socket.name + '');
+    user.messageUser('Type /help for commands');
 
-    users.messageUsers(socket.name + ' joined the chat\n');
+    user.messageUsers(socket.name + ' joined the chat\n');
 
     // Handle data
     socket.on('data', function (data) {
-        users.setData(data);
+        user.setData(data);
 
         if (commands.exists(data, '/name')) {
-            users.changeUserName();
+            user.changeUserName();
 
             return;
         } else if (commands.exists(data, '/exit')) {
-            users.removeUser();
+            user.removeUser();
 
             socket.end();
 
             return;
         } else if (commands.exists(data, '/help')) {
-            users.messageUser(' * /name newName\n * /exit (for exit)');
+            user.messageUser(' * /name newName\n * /exit (for exit)');
 
             return;
         } else {
-            users.messageUsers(socket.name + ' > ' + data);
+            user.messageUsers(socket.name + ' > ' + data);
         }
     });
 
     // Remove the client from the list when it leaves
     socket.on('end', function () {
-        users.removeUser();
+        user.removeUser();
 
-        users.messageUsers(socket.name + ' left the chat.');
+        user.messageUsers(socket.name + ' left the chat.');
     });
 }).listen(5000);
 
